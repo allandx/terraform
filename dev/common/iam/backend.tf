7d281a23129a517c7f2d54a20f744176fd488262
allancheo <allancheo@allancheos-MacBook-Air.local>
2023-12-05 18:16:53 +0800

    added backend.tf for iam parent module

---
+++ dev/common/iam/backend.tf
@@ -0,0 +1,9 @@
+# Comment out the below if you are working on local
+
+# terraform {
+#   backend "s3" {
+#     bucket = "tfstate-bucket-10"
+#     key    = "iam-user-group-policy.tfstate" #Change the value of this to <your suggested name>.tfstate for example
+#     region = "ap-southeast-1"
+#   }
+# }
